2c069ea646eaa5e6bd42e82af975f1ee216af328
qme1519 <jakubek.mi@gmail.com>
2021-06-19 21:35:17 +0300

    Dot

--- button/urls.py
+++ button/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.button_index, name="button_index"),
-    path("send_email", views.button_email, name="button_email")
+    path("send_email", views.button_email, name="button_email"),
 ]
