7550df958595c325df2a9f434f670d4bd47ae8f2
qme1519 <jakubek.mi@gmail.com>
2021-06-19 19:06:48 +0300

    revert

--- button/urls.py
+++ button/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.button_index, name="button_index")
+    path("", views.button_index, name="button_index"),
+    path("send_email", views.button_email, name="button_email")
 ]
-
-#    path("send_email", views.button_email, name="button_email")
