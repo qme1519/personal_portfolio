07f676dae3c549376730e11282852f699e1e7050
qme1519 <jakubek.mi@gmail.com>
2021-06-19 19:17:04 +0300

    Tidy up

--- button/views.py
+++ button/views.py
@@ -15,7 +15,7 @@ def button_index(request):
             if request.POST['choice'] == choice[0]:
                 what_she_needs = choice[1]
         send_mail(what_she_needs, "Napisz do niej :)", "me@jakub-michalski.tech", ["jakubek.mi@gmail.com"], fail_silently=False,)
-        button_email()
+        return button_email(request, context)
     else:
         form = Form()
         context = {'form':form}
